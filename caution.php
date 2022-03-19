11031b0e51cdb626b2d4941e35944957bbc71347
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2022-03-19 16:30:38 +0100

    emoji's

--- caution.php
+++ caution.php
@@ -3,7 +3,7 @@
 ?>
                
 <div class="description">
-    <h1>Voor dat we beginnen...</h1>
+    <h1>Voor dat we beginnen... 😬</h1>
     <p>
         In deze test wordt gevraagd naar persoonlijke ervaringen. Om dit voor jouw als proefpersoon comfortabel te houden,
         maken we gebruik van een techniek, waarbij de data die verzameld wordt, in eerste instantie alleen op jouw apparaat wordt opgeslagen.
